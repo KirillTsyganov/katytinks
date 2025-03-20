db0f050c0fd593f8c58d55854e074a344be6e37f
KirillTsyganov <kirill.tsyganov@outlook.com.au>
2025-03-20 23:38:03 +1100

    adding vue router and views

---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>About Page</h1>
+    <h2>Kirill Tsyganov</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'AboutView',
+})
+</script>
